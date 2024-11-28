1e00cf430b093f10ffc2fdf01f474fbeea3aae3c
omer-sabir03 <omer.sabir03@gmail.com>
2024-11-28 10:36:15 +0530

    netflix-gpt

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-red-500">
+      <div>Hello Everyone, How are you all</div>
+    </div>
+  );
+}
+
+export default App;
